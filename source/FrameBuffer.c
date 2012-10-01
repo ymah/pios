eca7a448aed3d6c673f0378877082a9439df2079
Jeremy Pereira <jeremyp@jeremyp.net>
2012-10-01 07:02:49 +0100

    Frame buffer postbox send/receive

--- source/FrameBuffer.c
+++ source/FrameBuffer.c
@@ -8,6 +8,21 @@
 
 #include "FrameBuffer.h"
 
+enum Bits
+{
+    STATUS_WRITE_READY_BIT = 31,
+    STATUS_READ_READY_BIT  = 30,
+    CHANNEL_BITS 		   = 4,
+};
+
+enum Masks
+{
+    STATUS_WRITE_READY_MASK = 1 << STATUS_WRITE_READY_BIT,
+    STATUS_READ_READY_MASK  = 1 << STATUS_READ_READY_BIT,
+    VALUE_MASK 				= ~0 << CHANNEL_BITS,
+    CHANNEL_MASK 			= ~VALUE_MASK,
+};
+
 struct FBPostBox
 {
 	volatile uint32_t read;
@@ -21,6 +36,41 @@ struct FBPostBox
 FBPostBox* fb_getFPBostBox()
 {
     return (FBPostBox*) 0x2000B880;
+}
+
+bool fb_send(FBPostBox* postbox, uint32_t message, uint32_t channel)
+{
+    bool ret =false;
+    if ((message & 0xF) == 0 && channel < FB_POSTBOX_CHANNELS)
+    {
+        while ((postbox->status & STATUS_WRITE_READY_MASK) != 0)
+        {
+            // spin
+        }
+        postbox->write = message | channel;
+    }
+    return ret;
+}
+
+uint32_t fb_read(FBPostBox* postbox, uint32_t channel)
+{
+    uint32_t ret = -1;
+    if (channel < FB_POSTBOX_CHANNELS)
+    {
+        uint32_t readValue = 0xFFFFFFFF;
+        uint32_t readChannel = 0x7FFFFFFF; // Requested channel will never be this
+        while (readChannel != channel)
+        {
+            while ((postbox->status & STATUS_READ_READY_MASK) != 0)
+            {
+                // spin
+            }
+            readValue = postbox->read;
+            readChannel = readValue & CHANNEL_MASK;
+        }
+        ret = readValue & VALUE_MASK;
+    }
+    return ret;
 }
 
 
--- source/FrameBuffer.h
+++ source/FrameBuffer.h
@@ -10,16 +10,55 @@
 #define BakingPi_FrameBuffer_h
 
 #include "bptypes.h"
+
+enum
+{
+    /*!
+     *  @brief Number of supported postbox channels for the CPU
+     */
+    FB_POSTBOX_CHANNELS = 8,
+};
 /*!
  *  @brief an opaque type that rerpesents the frame buffer postbox
  */
 struct FBPostBox;
 typedef struct FBPostBox FBPostBox;
 
+struct FrameBuffer;
+typedef struct FrameBuffer FrameBuffer;
+
 /*!
  *  @brief get the address of the frame buffer postbox
  *  @return The frame buffer postbox
  */
 FBPostBox* fb_getFPBostBox();
+
+/*!
+ *  @brief Low level send to mailbox.
+ *
+ *  This function works by spinning on a status flag until it can write the 
+ *  message.
+ *  @param postbox the address of the mailbox
+ *  @param message the message to write.  The low four bits must be zero.
+ *  @param channel the mail box channel - 0 to 7 at the moment
+ *  @return true if the parameters validate OK
+ */
+bool fb_send(FBPostBox* postbox, uint32_t message, uint32_t channel);
+
+/*!
+ *  @brief Read the postbox for data from the specified channel.
+ *
+ *  This function works by spinning on a status flag until it can read some
+ *  data.  This means that, if the GPU is not expecting to need to send 
+ *  something, this method will hang.  Also, any messages prior to the first
+ *  one for the specified channel will be discarded.
+ *  @param postbox Address of the GPU postbox.
+ *  @param channel Channel to read from, must be in the range 0-7.
+ *  @return The data on the channel or ((uint32_t)-1) if the parameters are 
+ *  incorrect.
+ *  Valid channel data will always have the low four bits clear.
+ */
+uint32_t fb_read(FBPostBox* postbox, uint32_t channel);
+
 
 #endif
